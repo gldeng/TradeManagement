3f4793b47a03970f51f5d19b9187e1fa504d25c4
gldeng <>
2018-03-21 12:30:36 +0800

    FIx site-map

--- scripts/app.py
+++ scripts/app.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from flask import jsonify
+from flask import jsonify, url_for
 
 dirname = os.path.abspath(os.path.dirname(__file__))
 
@@ -11,6 +11,12 @@ from trademan import create_app
 configfile = os.path.join(dirname, 'app.cfg')
 
 app = create_app(configfile)
+
+
+def has_no_empty_params(rule):
+    defaults = rule.defaults if rule.defaults is not None else ()
+    arguments = rule.arguments if rule.arguments is not None else ()
+    return len(defaults) >= len(arguments)
 
 
 @app.route("/site-map")
